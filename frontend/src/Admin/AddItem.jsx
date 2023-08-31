9a162952a63b79e16548ca340cfa730b1a6d55e2
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-08-31 22:18:35 +0530

    DAY-2

--- frontend/src/Admin/AddItem.jsx
+++ frontend/src/Admin/AddItem.jsx
@@ -1,134 +1,194 @@
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import styled from "styled-components"
+import WomenImages from "../Images/WomenImages.jpg"
 
 export const AddItem = () => {
-  return (
-    <DIV>
+    return (
+        <DIV>
 
-<div className='formParentDiv'>
+            <div className='formParentDiv'>
 
-<Text fontSize={'3xl'} as={'b'} textAlign={'center'}>Admin Login</Text>
+                <div className='imageParent'>
+                    <div>
+                        <img src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/5/l/g/l-pk19sh09n-surhi-original-imaft8ttegs2zxzh-bb.jpeg?q=70" alt="" />
+                    </div>
+                </div>
 
-<form action="">
-
-    <div>
-        <StyledInput type='email' placeholder='Email'  />
-    </div>
-
-    <div>
-    <StyledInput type='password' placeholder='Password'  />
-    </div>
-
-    <div className='btn' >
-        <Button colorScheme='blue' size='md' px={5} >
-            Login
-        </Button>
-    </div>
-
-</form>
-</div>
-
-<div className='formParentDiv'>
-
-<Text fontSize={'3xl'} as={'b'} textAlign={'center'}>Admin Login</Text>
-
-<form action="">
-
-    <div>
-        <StyledInput type='email' placeholder='Email'  />
-    </div>
-
-    <div>
-    <StyledInput type='password' placeholder='Password'  />
-    </div>
-
-    <div className='btn' >
-        <Button colorScheme='blue' size='md' px={5} >
-            Login
-        </Button>
-    </div>
-
-</form>
-</div>
+                <div className="container">
+                    <Text fontSize={'2xl'} as={'b'} style={{ textAlign: "center" }}>Men</Text>
+                </div>
+                {/* <p style={{textAlign: "center"}}>ddw</p> */}
 
 
-<div className='formParentDiv'>
+                <div className='btn' >
+                    <Button colorScheme='blue' size='md' px={5} >
+                        Add Items
+                    </Button>
+                </div>
 
-<Text fontSize={'3xl'} as={'b'} textAlign={'center'}>Admin Login</Text>
-
-<form action="">
-
-    <div>
-        <StyledInput type='email' placeholder='Email'  />
-    </div>
-
-    <div>
-    <StyledInput type='password' placeholder='Password'  />
-    </div>
-
-    <div className='btn' >
-        <Button colorScheme='blue' size='md' px={5} >
-            Login
-        </Button>
-    </div>
-
-</form>
-</div>
+                <div className='btn' >
+                    <Button colorScheme='green' size='md' px={5} >
+                        Edit Items
+                    </Button>
+                </div>
 
 
-<div className='formParentDiv'>
-
-<Text fontSize={'3xl'} as={'b'} textAlign={'center'}>Admin Login</Text>
-
-<form action="">
-
-    <div>
-        <StyledInput type='email' placeholder='Email'  />
-    </div>
-
-    <div>
-    <StyledInput type='password' placeholder='Password'  />
-    </div>
-
-    <div className='btn' >
-        <Button colorScheme='blue' size='md' px={5} >
-            Login
-        </Button>
-    </div>
-
-</form>
-</div>
+            </div>
 
 
 
-    </DIV>
-  )
+
+
+            <div className='formParentDiv'>
+
+
+                <div className='imageParent'>
+                    <div>
+
+                        <img src="https://rukminim2.flixcart.com/image/832/832/l13whow0/sari/z/r/s/free-lajamani-new-desirable-admirable-most-south-indian-bengali-original-imagcr2vmfydhzxe.jpeg?q=70" alt="" />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <Text fontSize={'2xl'} as={'b'} style={{ textAlign: "center" }}>Women</Text>
+                </div>
+                {/* <p style={{textAlign: "center"}}>ddw</p> */}
+
+
+                <div className='btn' >
+                    <Button colorScheme='blue' size='md' px={5} >
+                        Add Items
+                    </Button>
+                </div>
+
+
+                <div className='btn' >
+                    <Button colorScheme='green' size='md' px={5} >
+                        Edit Items
+                    </Button>
+                </div>
+            </div>
+
+
+            <div className='formParentDiv'>
+
+                <div className='imageParent'>
+                    <div>
+                        <img src="https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/p/l/j/-original-imagrnddj5yqfzzp.jpeg?q=70" alt="" />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <Text fontSize={'2xl'} as={'b'} style={{ textAlign: "center" }}>Shoes</Text>
+                </div>
+                {/* <p style={{textAlign: "center"}}>ddw</p> */}
+
+
+                <div className='btn' >
+                    <Button colorScheme='blue' size='md' px={5} >
+                        Add Items
+                    </Button>
+                </div>
+
+                <div className='btn' >
+                    <Button colorScheme='green' size='md' px={5} >
+                        Edit Items
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+
+            <div className='formParentDiv'>
+
+                <div className='imageParent'>
+                    <div>
+                        <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/mobile/k/w/k/-original-imagg2abzhxjckxu.jpeg?q=70" alt="" />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <Text fontSize={'2xl'} as={'b'} style={{ textAlign: "center" }}>Mobiles</Text>
+                </div>
+                {/* <p style={{textAlign: "center"}}>ddw</p> */}
+
+
+                <div className='btn' >
+                    <Button colorScheme='blue' size='md' px={5} >
+                        Add Items
+                    </Button>
+                </div>
+
+                <div className='btn' >
+                    <Button colorScheme='green' size='md' px={5} >
+                        Edit Items
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+
+            <div className='formParentDiv'>
+
+                <div className='imageParent'>
+                    <div>
+                        <img src="https://rukminim2.flixcart.com/image/832/832/xif0q/watch/k/e/l/1-ld-bk054-black-louis-devin-men-original-imagg25gzwqeawhv.jpeg?q=70" alt="" />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <Text fontSize={'2xl'} as={'b'} >Watches</Text>
+                </div>
+                {/* <p style={{textAlign: "center"}}>ddw</p> */}
+
+
+                <div className='btn' >
+                    <Button colorScheme='blue' size='md' px={5} >
+                        Add Items
+                    </Button>
+                </div>
+
+                <div className='btn' >
+                    <Button colorScheme='green' size='md' px={5} >
+                        Edit Items
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+
+        </DIV>
+    )
 }
 
 const DIV = styled.div`
 
 
 display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
+flex-wrap: wrap;
+/* justify-content: center; */
+/* align-items: center; */
+/* height: 100vh; */
 
 
 .formParentDiv{
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    padding: 20px;
+    /* box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; */
+    /* padding: 20px; */
     border-radius: 5px;
-}
-
-input{
-    padding: 5px;
-    margin-bottom: 10px;
-    border: 2px solid gray;
-}
-
-form{
-    margin-top: 15px;
+    /* height: 100vh;  */
+    margin: 20px;
+    width: 10%;
 }
 
 .btn{
@@ -137,6 +197,21 @@ form{
     margin-top: 10px;
     
 }
+
+.container {
+  display: flex;
+  justify-content: center; /* Horizontally center */
+  align-items: center; /* Vertically center */
+  margin-top: 10px;
+}
+
+.imageParent{
+    /* display: flex;
+    align-items: center;
+    justify-content: center; */
+    height: 204px;
+    /* border: 2px solid red; */
+  }
 
 
     
@@ -150,5 +225,6 @@ const StyledInput = styled.input`
     border-color: #0999ff; /* Change border color when focused */
     outline: none; /* Remove default focus outline */
   }
+
   `
 
